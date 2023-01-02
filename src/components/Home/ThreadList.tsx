2a6e435ce20af3ebe87ced11ceb150fe36462bbb
Kim-Jaemin420 <jany240@naver.com>
2023-01-02 20:32:31 +0900

    Modify: 분리한 ThreasListItem 컴포넌트를 사용

--- src/components/Home/ThreadList.tsx
+++ src/components/Home/ThreadList.tsx
@@ -1,8 +1,6 @@
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-
-import { formatDate } from "@/utils/formatDate";
-
 import { Thread } from "@/types/thread";
+
+import ThreadListItem from "@/components/common/thread/ThreadListItem";
 
 interface Props {
   threads: Thread[];
@@ -10,27 +8,9 @@ interface Props {
 
 const ThreadList = ({ threads }: Props) => {
   return (
-    <ul className="max-h-500pxr min-h-500pxr overflow-auto rounded-sm border border-t-0">
+    <ul className="max-h-500pxr min-h-500pxr overflow-auto rounded-sm border border-t-0 py-10pxr">
       {threads.map(({ _id, createdAt, title, author }) => (
-        <li key={_id} className="px-10pxr py-5pxr">
-          <div className="flex items-center">
-            <Avatar className="mr-3">
-              <AvatarImage src="/public/svg/userProfile.svg" alt="프로필" />
-              {/*TODO: 로그인/회원가입 추가시 옵셔널 삭제 예정 (2023.01.02)*/}
-              <AvatarFallback>{author.nickname?.charAt(0)}</AvatarFallback>
-            </Avatar>
-            <div className="min-w-0 flex-grow">
-              <div className="flex justify-between">
-                {/* TODO: 로그인/회원가입 추가시 기본값 "프롱이" 삭제 예정 (2023.01.02)*/}
-                <span className="text-lg font-semibold">{author.nickname || "프롱이"}</span>
-                <span className="text-gray-400">{formatDate(createdAt)}</span>
-              </div>
-              <div className="overflow-hidden truncate text-ellipsis pr-50pxr text-gray-500">
-                {title}
-              </div>
-            </div>
-          </div>
-        </li>
+        <ThreadListItem id={_id} createdAt={createdAt} title={title} author={author} />
       ))}
     </ul>
   );
