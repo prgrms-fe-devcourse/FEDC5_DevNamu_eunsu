1e4413c647c298c118d429e496e31f123f75ac5c
hun2__2 <wognskec@hanyang.ac.kr>
2023-12-28 21:36:33 +0900

    Feat: 멘션 선택
    1. input 하단에 자동완성으로 나온 멘션을 선택할 수 있다.
    2. input 상단에 선택된 멘션 리스트가 나온다.
    3. 리스트 아이템 클릭시 멘션이 해제된다.

--- src/components/Home/MentionInput.tsx
+++ src/components/Home/MentionInput.tsx
@@ -1,11 +1,15 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 import { Input } from "@/components/ui/input.tsx";
+import { Badge } from "@/components/ui/badge.tsx";
+import { Separator } from "@/components/ui/separator.tsx";
 
 import trie, { MyType } from "@/lib/trie.ts";
 
 const MentionInput = () => {
   const [mentionList, setMentionList] = useState<Array<MyType>>([]);
+  const [choiceList, setChoiceList] = useState<Array<MyType>>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const searchPeople = (e: FormEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -14,13 +18,50 @@ const MentionInput = () => {
     list && setMentionList(list);
   };
 
+  const handleAddChoiceList = (people: MyType) => {
+    if (!inputRef.current) return;
+    inputRef.current.value = "";
+    setChoiceList((prev) => [...prev, people]);
+    setMentionList([]);
+  };
+
+  const handleDeleteChoiceList = (people: MyType) => {
+    const newChoiceList = [...choiceList].filter(
+      ({ name, userId }) => !(name === people.name && userId === people.userId),
+    );
+    setChoiceList(newChoiceList);
+  };
+
   return (
     <div>
-      <Input type="text" onChange={searchPeople} />
-      {mentionList && (
-        <div>
-          {mentionList.map(({ name, userId }) => {
-            return <p key={userId}>{name}</p>;
+      {!!choiceList.length && (
+        <div className="mb-2 flex gap-2">
+          {choiceList.map(({ name, userId }) => {
+            return (
+              <Badge key={userId} onClick={() => handleDeleteChoiceList({ name, userId })}>
+                {name}
+              </Badge>
+            );
+          })}
+        </div>
+      )}
+
+      <Input type="text" onChange={searchPeople} ref={inputRef} />
+
+      {!!mentionList.length && (
+        <div className="mt-2 overflow-hidden scroll-auto border p-2">
+          {mentionList.map(({ name, userId }, idx) => {
+            return (
+              <div key={userId}>
+                <p
+                  onClick={() => handleAddChoiceList({ name, userId })}
+                  className="hover:font-bold"
+                >
+                  {name}
+                </p>
+                {idx !== mentionList.length - 1 && <Separator className="my-2" />}
+              </div>
+            );
           })}
         </div>
       )}
--- src/lib/trie.ts
+++ src/lib/trie.ts
@@ -6,6 +6,11 @@ const USER_LIST = [
   { name: "백윤서", userId: "3" },
   { name: "이재준", userId: "4" },
   { name: "조재훈", userId: "5" },
+  { name: "김성빈", userId: "6" },
+  { name: "김재민", userId: "7" },
+  { name: "백윤서", userId: "8" },
+  { name: "이재준", userId: "9" },
+  { name: "조재훈", userId: "10" },
 ];
 
 export type MyType = {
