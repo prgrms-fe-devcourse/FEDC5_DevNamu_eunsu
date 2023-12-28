4769ed3f9a4f5ed4e911b5f7d45807ec04afed3c
Kim-Jaemin420 <jany240@naver.com>
2023-12-29 00:09:17 +0900

    Feat: channel 관련 query key 생성

---
+++ src/apis/channel/queryKey.ts
@@ -0,0 +1,12 @@
+import { createQueryKeys } from "@lukemorales/query-key-factory";
+
+import { getChannels } from "./channel";
+
+const channels = createQueryKeys("channel", {
+  allChannels: {
+    queryKey: ["channels"],
+    queryFn: () => getChannels(),
+  },
+});
+
+export default channels;
