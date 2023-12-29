3ec77c35970ccee6c8f5e73dfb5b73c6f0ec9b0b
Kim-Jaemin420 <jany240@naver.com>
2023-12-30 02:32:39 +0900

    Style: ChannelMenu가 브라우저 크기에 맞도록 스타일 변경

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,8 @@ import ChannelMenu from "@/components/Home/ChannelMenu";
 
 const Home = () => {
   return (
-    <div className="mt-50pxr flex flex-col items-center justify-center">
-      <div className="w-980pxr">
+    <div className="mt-12 flex flex-col items-center justify-center">
+      <div className="w-full max-w-4xl px-4">
         <ChannelMenu />
       </div>
     </div>
