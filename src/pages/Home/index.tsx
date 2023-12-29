3588053a85f4be90c160600e7064bb64b218383a
Kim-Jaemin420 <jany240@naver.com>
2023-12-29 22:09:58 +0900

    Feat: 구현한 ChannelMenu를 홈페이지에서 사용

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
-const HomePage = () => {
-  return <div></div>;
+import ChannelMenu from "@/components/Home/ChannelMenu";
+
+const Home = () => {
+  return (
+    <div className="mt-50pxr grid place-items-center">
+      <div className="w-980pxr">
+        <ChannelMenu />
+      </div>
+    </div>
+  );
 };
 
-export default HomePage;
+export default Home;
