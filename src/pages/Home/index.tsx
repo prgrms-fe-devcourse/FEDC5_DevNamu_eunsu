090d57ad8675866d84413eaa330df9aef03ac71b
Kim-Jaemin420 <jany240@naver.com>
2023-12-30 20:30:33 +0900

    Feat: channel, thread 커스텀 훅을 사용하여 data fetch

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,18 @@
+import { useLocation } from "react-router-dom";
+
 import ChannelNavigationMenu from "@/components/Home/ChannelNavigationMenu";
+import useChannelDetailsQuery from "@/apis/channel/useChannelDetailsQuery";
+import useThreadsQuery from "@/apis/thread/useThreadsQuery";
 
 const HomePage = () => {
+  const location = useLocation();
+
+  const channelName = location.pathname.split("/").pop() || "compliment";
+  const channelQuery = useChannelDetailsQuery(channelName);
+  const threadsQuery = useThreadsQuery(channelQuery.channelDetails?._id);
+
+  console.log(threadsQuery);
+
   return (
     <div className="mt-12 flex flex-col items-center justify-center">
       <div className="w-full max-w-4xl px-4">
