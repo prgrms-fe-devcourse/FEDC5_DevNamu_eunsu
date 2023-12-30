3626f79fca8c35faf8ab483d58b82965163152b2
Kim-Jaemin420 <jany240@naver.com>
2023-12-30 20:27:22 +0900

    Modify: channel id가 undefined일 경우 요청하지 않도록 구현
    enabled와 channel id가 undefined 이면 queryFn 호출하지 않음.

--- src/apis/thread/queryKey.ts
+++ src/apis/thread/queryKey.ts
@@ -3,9 +3,10 @@ import { createQueryKeys } from "@lukemorales/query-key-factory";
 import { getThreadsByChannelId } from "./queryFn";
 
 const threads = createQueryKeys("thread", {
-  threadsByChannel: (channelId: string) => ({
+  threadsByChannel: (channelId: string | undefined) => ({
     queryKey: ["threads", channelId],
-    queryFn: () => getThreadsByChannelId(channelId),
+    queryFn: channelId ? () => getThreadsByChannelId(channelId) : undefined,
+    enabled: !!channelId,
   }),
 });
 
