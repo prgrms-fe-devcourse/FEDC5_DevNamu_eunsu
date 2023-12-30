c9effd6e596746ea348596a5b1abe057e205a93c
Kim-Jaemin420 <jany240@naver.com>
2023-12-30 15:31:38 +0900

    Feat: 특정 channel 정보 가져오는 api 쿼리키 생성

--- src/apis/channel/queryKey.ts
+++ src/apis/channel/queryKey.ts
@@ -1,12 +1,16 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
-import { getChannels } from "./queryFn";
+import { getChannelById, getChannels } from "./queryFn";
 
 const channels = createQueryKeys("channel", {
   allChannels: {
     queryKey: ["channels"],
     queryFn: getChannels,
   },
+  channelById: (id: string) => ({
+    queryKey: ["channels", id],
+    queryFn: () => getChannelById(id),
+  }),
 });
 
 export default channels;
