d63a2f3617e99250292b4ff37dba150fd984c401
hun2__2 <wognskec@hanyang.ac.kr>
2023-12-31 22:33:04 +0900

    Modify: 멘션된 리스트와 자동완성리스트 분리

--- src/components/Home/MentionInput.tsx
+++ src/components/Home/MentionInput.tsx
@@ -10,6 +10,7 @@ const MentionInput = () => {
   const [mentionList, setMentionList] = useState<Array<MyType>>([]);
   const [choiceList, setChoiceList] = useState<Array<MyType>>([]);
   const inputRef = useRef<HTMLInputElement>(null);
+  const focusRef = useRef<HTMLElement[]>([]);
 
   const searchPeople = (e: FormEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -34,39 +35,57 @@ const MentionInput = () => {
 
   return (
     <div>
-      {!!choiceList.length && (
-        <div className="mb-2 flex gap-2">
-          {choiceList.map(({ name, userId }) => {
-            return (
-              <Badge key={userId} onClick={() => handleDeleteChoiceList({ name, userId })}>
-                {name}
-              </Badge>
-            );
-          })}
-        </div>
-      )}
+      {!!choiceList.length && <ChoiceList list={choiceList} onClick={handleDeleteChoiceList} />}
 
-      <Input type="text" onChange={searchPeople} ref={inputRef} />
+      <Input
+        type="text"
+        onChange={searchPeople}
+        ref={inputRef}
+        placeholder="멘션할 대상을 선택해주세요."
+      />
 
-      {!!mentionList.length && (
-        <div className="mt-2 overflow-hidden scroll-auto border p-2">
-          {mentionList.map(({ name, userId }, idx) => {
-            return (
-              <div key={userId}>
-                <p
-                  onClick={() => handleAddChoiceList({ name, userId })}
-                  className="hover:font-bold"
-                >
-                  {name}
-                </p>
-                {idx !== mentionList.length - 1 && <Separator className="my-2" />}
-              </div>
-            );
-          })}
-        </div>
-      )}
+      {!!mentionList.length && <MentionList list={mentionList} onClick={handleAddChoiceList} />}
     </div>
   );
 };
 
 export default MentionInput;
+
+interface ListProps {
+  list: MyType[];
+  onClick: (people: MyType) => void;
+}
+
+const MentionList = ({ list, onClick }: ListProps) => {
+  return (
+    <div className="mt-2 overflow-hidden scroll-auto border p-2">
+      {list.map(({ name, userId }, idx) => {
+        return (
+          <div key={userId} className="hover:bg-gray-100 focus:bg-gray-100">
+            <p
+              onClick={() => onClick({ name, userId })}
+              className="cursor-pointer py-2 hover:font-bold focus:font-bold"
+            >
+              {name}
+            </p>
+            {idx !== list.length - 1 && <Separator />}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const ChoiceList = ({ list, onClick }: ListProps) => {
+  return (
+    <div className="mb-2 flex gap-2">
+      {list.map(({ name, userId }) => {
+        return (
+          <Badge key={userId} onClick={() => onClick({ name, userId })} className="cursor-pointer">
+            {name}
+          </Badge>
+        );
+      })}
+    </div>
+  );
+};
