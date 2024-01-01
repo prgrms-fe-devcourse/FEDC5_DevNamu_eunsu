8c5f12dd31bd0bb5684a753f15a5ddf8afa66141
Kim-Jaemin420 <jany240@naver.com>
2024-01-01 10:35:17 +0900

    Feat: ThreadList 컴포넌트를 사용하여 thread 렌더링

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import ChannelNavigationMenu from "@/components/Home/ChannelNavigationMenu";
 import useChannelDetailsQuery from "@/apis/channel/useChannelDetailsQuery";
 import useThreadsQuery from "@/apis/thread/useThreadsQuery";
+import ThreadList from "@/components/Home/ThreadList";
 
 const HomePage = () => {
   const location = useLocation();
@@ -11,13 +12,12 @@ const HomePage = () => {
   const channelQuery = useChannelDetailsQuery(channelName);
   const threadsQuery = useThreadsQuery(channelQuery.channelDetails?._id);
 
-  console.log(threadsQuery);
-
   return (
     <div className="mt-12 flex flex-col items-center justify-center">
       <div className="w-full max-w-4xl px-4">
         <ChannelNavigationMenu />
       </div>
+      <div>{threadsQuery.threads && <ThreadList threads={threadsQuery.threads} />}</div>
     </div>
   );
 };
