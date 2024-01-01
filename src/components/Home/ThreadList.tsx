193954a48390ba80b83921451858b3209b486385
Kim-Jaemin420 <jany240@naver.com>
2024-01-01 13:43:53 +0900

    Feat: list ui 구현

--- src/components/Home/ThreadList.tsx
+++ src/components/Home/ThreadList.tsx
@@ -1,3 +1,5 @@
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+
 import { Thread } from "@/types/thread";
 
 interface Props {
@@ -8,8 +10,22 @@ const ThreadList = ({ threads }: Props) => {
   return (
     <ul>
       {threads.map((thread) => (
-        <li key={thread._id}>
-          <span>{thread.title}</span>
+        <li key={thread._id} className="border">
+          <div className="flex items-center">
+            <Avatar className="mr-5pxr">
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="max-w-xl">
+              <div>
+                <span className="text-lg font-semibold">익명의 프롱이</span>
+                <span>12:33</span>
+              </div>
+              <span className="block overflow-hidden truncate text-ellipsis text-gray-500">
+                {thread.title}
+              </span>
+            </div>
+          </div>
         </li>
       ))}
     </ul>
