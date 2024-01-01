1e81fa68e0b82c577706f80cf4f90d03854cf84b
Kim-Jaemin420 <jany240@naver.com>
2024-01-01 15:31:56 +0900

    Feat: user response type 선언

---
+++ src/types/user.ts
@@ -0,0 +1,23 @@
+import { Follow, Message } from "./notification";
+import { Like, Thread } from "./thread";
+
+export interface User {
+  coverImage: string;
+  image: string;
+  role: string;
+  emailVerified: boolean;
+  banned: boolean;
+  isOnline: boolean;
+  posts: Thread[];
+  likes: Like[];
+  comments: string[];
+  followers: [];
+  following: Follow[];
+  notifications: Notification[];
+  messages: Message[];
+  _id: string;
+  fullName: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+}
