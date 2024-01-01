4b2db73974e22194c22a8112dd213e2afba96d6d
J'aimemin <61978339+Kim-Jaemin420@users.noreply.github.com>
2024-01-01 11:52:37 +0900

    [#15] Feat: 홈 페이지의 채널 메뉴 UI 구현 (#18)

---
+++ src/components/Home/ChannelNavigationMenu.tsx
@@ -0,0 +1,45 @@
+import { NavLink } from "react-router-dom";
+
+import { cn } from "@/lib/utils";
+
+const getNavLinkClass = (isActive: boolean) => {
+  cn(
+    `text-gray-600 rounded-t-md hover:bg-gray-100 hover:text-gray-800 text-center text-lg font-medium px-4 py-3 border border-b-0`,
+    isActive ? "bg-gray-100" : "",
+  );
+};
+
+const ChannelNavigationMenu = () => {
+  return (
+    <nav className="border-b-2 border-gray-500 py-2">
+      <ul className="flex">
+        <li>
+          <NavLink
+            to="/channels/compliment"
+            className={({ isActive }) => `${getNavLinkClass(isActive)}`}
+          >
+            칭찬 게시판
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/channels/cheering"
+            className={({ isActive }) => `${getNavLinkClass(isActive)}`}
+          >
+            응원 게시판
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/channels/incompetent"
+            className={({ isActive }) => `${getNavLinkClass(isActive)}`}
+          >
+            무능 게시판
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default ChannelNavigationMenu;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
+import ChannelNavigationMenu from "@/components/Home/ChannelNavigationMenu";
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className="mt-12 flex flex-col items-center justify-center">
+      <div className="w-full max-w-4xl px-4">
+        <ChannelNavigationMenu />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
