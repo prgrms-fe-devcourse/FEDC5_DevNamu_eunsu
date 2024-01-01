5decc2445a6a41476690535ddb6a42f10bc64c09
Kim-Jaemin420 <jany240@naver.com>
2024-01-01 21:10:15 +0900

    Feat: fullname 파싱하는 함수 구현

--- src/apis/thread/queryKey.ts
+++ src/apis/thread/queryKey.ts
@@ -1,12 +1,35 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
+import { Thread } from "@/types/thread";
+
 import { getThreadsByChannelId } from "./queryFn";
+
+const parseFullName = (fullName: string) => {
+  try {
+    return JSON.parse(fullName);
+  } catch (error) {
+    console.error("Error parsing fullName:", error);
+    return { name: "", nickname: "프롱이" };
+  }
+};
 
 const threads = createQueryKeys("thread", {
   threadsByChannel: (channelId: string | undefined) => ({
     queryKey: ["threads", channelId],
     queryFn: channelId ? () => getThreadsByChannelId(channelId) : undefined,
     enabled: !!channelId,
+    select: (data: Thread[]) =>
+      data.map((thread) => {
+        const parsedFullName = parseFullName(thread.author.fullName);
+        return {
+          ...thread,
+          author: {
+            ...thread.author,
+            name: parsedFullName.user,
+            nickname: parsedFullName.nickname,
+          },
+        };
+      }),
   }),
 });
 
