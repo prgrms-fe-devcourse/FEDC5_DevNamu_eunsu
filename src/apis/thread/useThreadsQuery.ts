999023b84ef520b5e7316e46b61a1aecdafcaff8
Kim-Jaemin420 <jany240@naver.com>
2024-01-01 10:34:43 +0900

    Feat: useQuery에 반환 타입(Thread[])지정

--- src/apis/thread/useThreadsQuery.ts
+++ src/apis/thread/useThreadsQuery.ts
@@ -1,9 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 
+import { Thread } from "@/types/thread";
+
 import threads from "./queryKey";
 
 const useThreadsQuery = (channelId: string | undefined) => {
-  const { data, isLoading, isError, error } = useQuery(threads.threadsByChannel(channelId));
+  const { data, isLoading, isError, error } = useQuery<Thread[]>(
+    threads.threadsByChannel(channelId),
+  );
 
   return {
     threads: data,
