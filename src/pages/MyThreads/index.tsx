3ecf83b2334d12f633405f4c985a33340347060d
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-01 09:44:10 +0900

    Feat: 내가 쓴 글 index 컴포넌트 설정
    Co-authored-by: yoon <bgyoons@users.noreply.github.com>

--- src/pages/MyThreads/index.tsx
+++ src/pages/MyThreads/index.tsx
@@ -1,5 +1,13 @@
+import ThreadHeader from "@/components/MyThreads/ThreadHeader";
+import MyThreadBody from "@/components/MyThreads/ThreadBody";
+
 const MyThreadsPage = () => {
-  return <div></div>;
+  return (
+    <div className="p-30pxr">
+      <ThreadHeader />
+      <MyThreadBody />
+    </div>
+  );
 };
 
 export default MyThreadsPage;
