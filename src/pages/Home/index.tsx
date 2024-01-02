a572b5b90338245e44e6eaf2a029f2c7ad37e74b
Kim-Jaemin420 <jany240@naver.com>
2024-01-02 11:49:39 +0900

    Modify: useThreadsByChannel을 사용하도록 변경

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,16 @@
-import { useLocation } from "react-router-dom";
-
 import ChannelNavigationMenu from "@/components/Home/ChannelNavigationMenu";
-import useChannelDetailsQuery from "@/apis/channel/useChannelDetailsQuery";
-import useThreadsQuery from "@/apis/thread/useThreadsQuery";
 import ThreadList from "@/components/Home/ThreadList";
+import useThreadsByChannel from "@/hooks/api/useThreadsByChannel";
 
 const HomePage = () => {
-  const location = useLocation();
-
-  const channelName = location.pathname.split("/").pop() || "compliment";
-  const channelQuery = useChannelDetailsQuery(channelName);
-  const threadsQuery = useThreadsQuery(channelQuery.channelDetails?._id);
+  const { threads } = useThreadsByChannel();
 
   return (
     <div className="mt-12 flex flex-col items-center justify-center">
       <div className="w-full max-w-4xl px-4">
         <ChannelNavigationMenu />
       </div>
-      <main className="w-full max-w-4xl px-4">
-        {threadsQuery.threads && <ThreadList threads={threadsQuery.threads} />}
-      </main>
+      <main className="w-full max-w-4xl px-4">{threads && <ThreadList threads={threads} />}</main>
     </div>
   );
 };
