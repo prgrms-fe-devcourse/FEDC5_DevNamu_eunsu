a261150b438aabec514b6661b440fd31880e1662
Kim-Jaemin420 <jany240@naver.com>
2024-01-02 12:26:06 +0900

    Rename: 바뀐 파일명으로 import

--- src/apis/thread/queryKey.ts
+++ src/apis/thread/queryKey.ts
@@ -1,6 +1,6 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
-import { parseFullName } from "@/utils/\bparsingJson";
+import { parseFullName } from "@/utils/parsingJson";
 
 import { Thread } from "@/types/thread";
 
