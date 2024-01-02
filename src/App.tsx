5885202b0e02c639289bfe45908b571a66a44b7d
hun2__2 <wognskec@hanyang.ac.kr>
2024-01-02 20:52:34 +0900

    Cleanup: 데모 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import MyNotificationsPage from "./pages/MyNotifications";
 import NotFoundPage from "./pages/NotFound";
 import Layout from "./components/Layout";
 
-import ExTextArea from "@/components/Demo/ExTextArea.tsx";
-
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -22,8 +20,6 @@ const App = () => (
           <Route path="channels/*" element={<HomePage />} />
           <Route path="my-threads" element={<MyThreadsPage />} />
           <Route path="my-notifications" element={<MyNotificationsPage />} />
-          {/*TODO: [24/1/2] demo 페이지는 나중에 지울 것*/}
-          <Route path="demo" element={<ExTextArea />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
