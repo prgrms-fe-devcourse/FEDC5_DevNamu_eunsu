4999c2c5b05936de4db37ecba56072d3ff0e83c0
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-03 09:57:32 +0900

    Refactor: useGetMyThread내 return 로직 변경

--- src/apis/mythreads/useMyThreadQuery.ts
+++ src/apis/mythreads/useMyThreadQuery.ts
@@ -3,11 +3,10 @@ import { useQuery } from "@tanstack/react-query";
 import { getMyThreads } from "./queryFn";
 
 const useGetMyThread = (userId: string) => {
-  const result = useQuery({
+  return useQuery({
     queryKey: ["my-threads", userId],
     queryFn: () => getMyThreads(userId),
   });
-  return result;
 };
 
 export default useGetMyThread;
