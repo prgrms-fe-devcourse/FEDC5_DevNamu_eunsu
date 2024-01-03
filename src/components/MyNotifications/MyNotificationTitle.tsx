3fa8f34c915307693c10451d27f8895cb2c74446
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-03 22:46:33 +0900

    Refactor: 내 알림 title 태그 수정

--- src/components/MyNotifications/MyNotificationTitle.tsx
+++ src/components/MyNotifications/MyNotificationTitle.tsx
@@ -2,7 +2,7 @@ const MyNotificationTitle = () => {
   return (
     <div>
       <div className="flex items-center gap-6 pb-4">
-        <p className="h-[30px] text-2xl font-bold">내 알림</p>
+        <span className="h-[30px] text-2xl font-bold">내 알림</span>
       </div>
       <hr />
     </div>
