cbabed31dbc8b9ab62cb75449ae7681e128a6dda
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-03 22:31:05 +0900

    Feat: 내 알림 index 및 title 컴포넌트 구현

---
+++ src/components/MyNotifications/MyNotificationTitle.tsx
@@ -0,0 +1,12 @@
+const MyNotificationTitle = () => {
+  return (
+    <div>
+      <div className="flex items-center gap-6 pb-4">
+        <p className="h-[30px] text-2xl font-bold">내 알림</p>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default MyNotificationTitle;
