e3088a44adc138f7de96d7ee649f65251603ecc4
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-03 20:12:44 +0900

    Modify: 유저 정보 갖고올 위치 미선정으로 인한 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
 
 import HomePage from "./pages/Home";
 import MyThreadsPage from "./pages/MyThreads";
 import MyNotificationsPage from "./pages/MyNotifications";
 import NotFoundPage from "./pages/NotFound";
 import Layout from "./components/Layout";
-import useUserStore from "./stores/user";
-import useGetUserInfo from "./apis/auth/useGetUserInfo";
-import { getLocalStorage } from "./utils/localStorage";
 
-const App = () => {
-  const { user, updateUser } = useUserStore();
-  const token = getLocalStorage("token", "");
-  const userInfo = useGetUserInfo(user === null, token);
-
-  useEffect(() => {
-    if (!userInfo) return;
-    if (!user) updateUser(userInfo);
-  }, [userInfo, updateUser, user]);
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="channels/*" element={<HomePage />} />
-          <Route path="my-threads" element={<MyThreadsPage />} />
-          <Route path="my-notifications" element={<MyNotificationsPage />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="channels/*" element={<HomePage />} />
+        <Route path="my-threads" element={<MyThreadsPage />} />
+        <Route path="my-notifications" element={<MyNotificationsPage />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
