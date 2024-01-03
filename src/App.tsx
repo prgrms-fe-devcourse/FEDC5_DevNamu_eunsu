1156a31b494bbea25096ea7d328531abc2654b9a
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-03 11:17:56 +0900

    Feat: 스토어에 유저 정보가 없는 경우 유저 정보 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
 
 import HomePage from "./pages/Home";
 import MyThreadsPage from "./pages/MyThreads";
 import MyNotificationsPage from "./pages/MyNotifications";
 import NotFoundPage from "./pages/NotFound";
 import Layout from "./components/Layout";
+import useUserStore from "./stores/user";
+import useGetUserInfo from "./apis/auth/useGetUserInfo";
+import { getLocalStorage } from "./utils/localStorage";
 
 const App = () => {
+  const { user, updateUser } = useUserStore();
+  const token = getLocalStorage("token", "");
+  const userInfo = useGetUserInfo(user === null, token);
 
+  useEffect(() => {
+    if (!userInfo) return;
+    if (!user) updateUser(userInfo);
+  }, [userInfo, updateUser, user]);
+
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
