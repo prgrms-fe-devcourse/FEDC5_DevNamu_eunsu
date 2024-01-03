a309ecd5d741e32274d48b350a75cddd1c88bd21
Kim-Jaemin420 <jany240@naver.com>
2024-01-03 13:04:34 +0900

    Feat: 스레드 리스트 고유 키 설정

--- src/components/Home/ThreadList.tsx
+++ src/components/Home/ThreadList.tsx
@@ -10,7 +10,7 @@ const ThreadList = ({ threads }: Props) => {
   return (
     <ul className="max-h-500pxr min-h-500pxr overflow-auto rounded-sm border border-t-0 py-10pxr">
       {threads.map(({ _id, createdAt, title, author }) => (
-        <ThreadListItem id={_id} createdAt={createdAt} title={title} author={author} />
+        <ThreadListItem key={_id} id={_id} createdAt={createdAt} title={title} author={author} />
       ))}
     </ul>
   );
