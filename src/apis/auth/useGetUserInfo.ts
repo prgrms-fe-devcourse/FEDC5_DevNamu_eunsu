f9e5aa25363783b4133b3759ca119389f0339e37
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-03 20:11:02 +0900

    Modify: 유저 정보 스토어에 저장하는 로직 삭제

--- src/apis/auth/useGetUserInfo.ts
+++ src/apis/auth/useGetUserInfo.ts
@@ -1,16 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { parseFullName } from "@/utils/parsingJson";
 
 import auth from "./queryKey";
 
 const useGetUserInfo = (isInvalidUserId: boolean, token: string) => {
-  const { data, isLoading } = useQuery(auth.userInfo(isInvalidUserId, token));
-  if (!data || isLoading) return;
-
-  const { _id: id, email, fullName } = data;
-  const { name, nickname } = parseFullName(fullName);
-  return { id, email, name, nickname, token, isLoggedIn: true };
+  return useQuery(auth.userInfo(isInvalidUserId, token));
 };
 
 export default useGetUserInfo;
