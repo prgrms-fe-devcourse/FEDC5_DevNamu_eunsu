c433f3574698699288bc081e81b4dc6c6b605e98
Kim-Jaemin420 <jany240@naver.com>
2024-01-04 12:13:11 +0900

    Feat: title json 파싱하는 함수 구현

--- src/utils/parsingJson.ts
+++ src/utils/parsingJson.ts
@@ -6,3 +6,12 @@ export const parseFullName = (fullName: string) => {
     return { name: "", nickname: "프롱이" };
   }
 };
+
+export const parseTitle = (title: string) => {
+  try {
+    return JSON.parse(title);
+  } catch (error) {
+    console.error("Error parsing title:", error);
+    return { content: "", nickname: "익명의 프롱이" };
+  }
+};
