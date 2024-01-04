9deb5712381c052fce7e17c0b69784c822c8ab87
Kim-Jaemin420 <jany240@naver.com>
2024-01-04 10:45:05 +0900

    Feat: editor textarea 컴포넌트 사용

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
 import ChannelNavigationMenu from "@/components/Home/ChannelNavigationMenu";
 import ThreadList from "@/components/Home/ThreadList";
+import EditorTextArea from "@/components/common/EditorTextArea";
 import useThreadsByChannel from "@/hooks/api/useThreadsByChannel";
 
 const HomePage = () => {
-  const { threads } = useThreadsByChannel();
+  const { threads, channelId } = useThreadsByChannel();
 
   return (
     <div className="mt-12 flex flex-col items-center justify-center">
@@ -11,6 +12,16 @@ const HomePage = () => {
         <ChannelNavigationMenu />
       </div>
       <main className="w-full max-w-4xl px-4">{threads && <ThreadList threads={threads} />}</main>
+      <div className="w-full max-w-4xl px-4">
+        <EditorTextArea
+          isMention={false}
+          contentType="post"
+          submitType="create"
+          postId=""
+          channelId={channelId}
+          nickName=""
+        />
+      </div>
     </div>
   );
 };
