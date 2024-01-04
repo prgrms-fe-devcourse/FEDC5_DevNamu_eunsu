ecffa954bb069963db78368b8e70f24c1c235a48
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-04 10:42:22 +0900

    [#54] Feat: 내 알림 더미데이터 및 UI 구현 (#57)

---
+++ src/components/MyNotifications/DummyContent.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  title: string;
+}
+
+const DummyContent = ({ title }: Props) => {
+  return (
+    <li className="max-w-xl">
+      <div className="mb-2 overflow-hidden truncate text-lg font-normal">
+        {title ? title : "글 내용이 존재하지 않음."}
+      </div>
+    </li>
+  );
+};
+
+export default DummyContent;
---
+++ src/components/MyNotifications/MyNotificationTitle.tsx
@@ -0,0 +1,12 @@
+const MyNotificationTitle = () => {
+  return (
+    <div>
+      <div className="flex items-center gap-6 pb-4">
+        <span className="h-[30px] text-2xl font-bold">내 알림</span>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default MyNotificationTitle;
