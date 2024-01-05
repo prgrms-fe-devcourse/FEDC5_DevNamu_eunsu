21755c2ab5b3aeaacb1d202988ab37c4eea80a32
hun2__2 <wognskec@hanyang.ac.kr>
2024-01-05 21:29:59 +0900

    Cleanup: demo 삭제 (#74)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Demo from "@/components/Demo.tsx";
-
 import HomePage from "./pages/Home";
 import MyThreadsPage from "./pages/MyThreads";
 import MyNotificationsPage from "./pages/MyNotifications";
@@ -16,8 +14,6 @@ const App = () => (
         <Route path="channels/*" element={<HomePage />} />
         <Route path="my-threads" element={<MyThreadsPage />} />
         <Route path="my-notifications" element={<MyNotificationsPage />} />
-        {/*TODO : [24/1/4] merge 시 데모 삭제 */}
-        <Route path="demo" element={<Demo />} />
       </Route>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
