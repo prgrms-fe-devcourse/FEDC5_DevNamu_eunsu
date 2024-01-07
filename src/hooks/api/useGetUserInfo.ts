f61bb4835422d2433752117ae35e8cce40ed2497
Kim-Jaemin420 <jany240@naver.com>
2024-01-08 02:43:04 +0900

    Modify: return 타입 user로 선언

--- src/hooks/api/useGetUserInfo.ts
+++ src/hooks/api/useGetUserInfo.ts
@@ -2,11 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 
 import { getLocalStorage } from "@/utils/localStorage";
 
+import { User } from "@/types/user";
+
 import { authKeys } from "./queryKeyFactory";
 
 const useGetUserInfo = () => {
   const token = getLocalStorage("token", "");
-  const { data, isPending } = useQuery({
+  const { data, isPending } = useQuery<User>({
     queryKey: authKeys.userInfo(token),
   });
 
