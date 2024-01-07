95ee9c700ea4e973a60993e9a000caf304794693
Kim-Jaemin420 <jany240@naver.com>
2024-01-08 02:35:31 +0900

    Modify: 알림 쿼리키 제거 및 파일 분리

--- src/hooks/api/useNotificationMutate.ts
+++ src/hooks/api/useNotificationMutate.ts
@@ -1,6 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 
-import { createNotification } from "@/apis/notification/queryFn.ts";
+import { createNotification } from "@/apis/notification";
 
 export const usePostNotification = () => {
   // todo [24/1/2] : error handling 공통으로 뺼 것
