88981128c08170fd9100bb0fae84ac9cf404ce05
Kim-Jaemin420 <jany240@naver.com>
2024-01-08 02:34:54 +0900

    Modify: mythread 쿼리키 제거 및 파일 분리

--- src/hooks/api/useMyThreadQuery.ts
+++ src/hooks/api/useMyThreadQuery.ts
@@ -1,12 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { getMyThreads } from "./queryFn";
+import { getMyThreads } from "@/apis/myThreads";
 
 const useGetMyThread = (userId: string) => {
   const { data, isPending } = useQuery({
     queryKey: ["my-threads", userId],
     queryFn: () => getMyThreads(userId),
   });
+
   return {
     myThreads: data,
     isPending,
