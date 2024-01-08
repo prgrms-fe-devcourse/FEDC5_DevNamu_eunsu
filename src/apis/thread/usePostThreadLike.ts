9c01e7c75f2c840f5c3a69a8727195cdbc72d7d0
Kim-Jaemin420 <jany240@naver.com>
2024-01-08 12:24:31 +0900

    Feat: invalid queries 추가

--- src/apis/thread/usePostThreadLike.ts
+++ src/apis/thread/usePostThreadLike.ts
@@ -1,10 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { postThreadLike } from "./queryFn";
+import threads from "./queryKey";
 
-const usePostThreadLike = () => {
+const usePostThreadLike = (channelId: string) => {
+  const queryClient = useQueryClient();
+
   const { mutate, isPending, isError } = useMutation({
     mutationFn: (postId: string) => postThreadLike(postId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: threads.threadsByChannel(channelId).queryKey,
+      });
+    },
   });
 
   return {
