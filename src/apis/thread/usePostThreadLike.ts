83d446614b9bf58d8010cdd8e44e2982c285410f
Kim-Jaemin420 <jany240@naver.com>
2024-01-08 12:40:57 +0900

    Feat: 좋아요 optimistic updates 추가

--- src/apis/thread/usePostThreadLike.ts
+++ src/apis/thread/usePostThreadLike.ts
@@ -1,5 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import { Thread } from "@/types/thread";
+
 import { postThreadLike } from "./queryFn";
 import threads from "./queryKey";
 
@@ -8,6 +10,31 @@ const usePostThreadLike = (channelId: string) => {
 
   const { mutate, isPending, isError } = useMutation({
     mutationFn: (postId: string) => postThreadLike(postId),
+    onMutate: async (postId) => {
+      await queryClient.cancelQueries({
+        queryKey: threads.threadsByChannel(channelId).queryKey,
+      });
+
+      const previousThreads = queryClient.getQueryData(
+        threads.threadsByChannel(channelId).queryKey,
+      );
+
+      queryClient.setQueryData(
+        threads.threadsByChannel(channelId).queryKey,
+        (oldThreads: Thread[]) =>
+          oldThreads.map((thread) =>
+            thread._id === postId ? { ...thread, likes: [...thread.likes, postId] } : thread,
+          ),
+      );
+
+      return { previousThreads };
+    },
+    onError: (error, postId, context) => {
+      queryClient.setQueryData(
+        threads.threadsByChannel(channelId).queryKey,
+        context?.previousThreads,
+      );
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: threads.threadsByChannel(channelId).queryKey,
