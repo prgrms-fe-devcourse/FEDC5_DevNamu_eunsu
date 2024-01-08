aa4cf374dc39c2c87cb357e9a06274f64418f55f
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-08 14:51:13 +0900

    Feat: 내 알림 내부에 api 데이터로 멘션과 댓글이 출력되도록 기능 구현
    Co-authored-by: hun2__2 <wognskec@hanyang.ac.kr>

--- src/components/MyNotifications/MyNotificationBody.tsx
+++ src/components/MyNotifications/MyNotificationBody.tsx
@@ -1,23 +1,35 @@
 import MyNotificationItem from "./MyNotificationItem";
 
-import useGetmyNotification from "@/apis/mynotification/useGetMyNotification";
+import useGetNotification from "@/apis/mynotification/useGetNotification";
+import useGetMentioned from "@/apis/mynotification/useGetMentioned";
 
 const MyNotificationBody = () => {
-  const { myNotifications, isPending } = useGetmyNotification();
+  const { myNotifications, isPending: notificationPending } = useGetNotification();
+  const { myMentions, isPending: mentionPending } = useGetMentioned();
 
-  if (isPending || !myNotifications) {
+  if (notificationPending || mentionPending) {
     return <span>Loading...</span>;
   }
+
+  console.log(notificationPending);
+  console.log(myMentions);
+
+  const 합쳐진데이터 = [...myNotifications, ...myMentions].sort(
+    (a, b) => new Date(a.createdAt) - new Date(b.createdAt),
+  );
+
+  console.log("합쳐진 데이터", 합쳐진데이터);
 
   return (
     <main className="p-10pxr">
       <ul className="list-none">
-        {myNotifications.map(({ _id, seen, comment, createdAt }) => {
+        {합쳐진데이터?.map(({ _id, comment, message, post, createdAt }) => {
           return (
             <MyNotificationItem
-              key={_id}
-              seen={seen}
+              key={typeof _id === "string" ? _id : _id[0]}
+              postId={post}
               comment={comment?.comment}
+              message={message}
               createdAt={createdAt}
             />
           );
--- src/components/MyNotifications/MyNotificationItem.tsx
+++ src/components/MyNotifications/MyNotificationItem.tsx
@@ -3,32 +3,58 @@ import { Separator } from "@/components/ui/separator";
 import { formatDate } from "@/utils/formatDate";
 
 interface Props {
-  seen: boolean;
-  post?: string;
+  postId: string;
   comment?: string;
   follow?: string;
   message?: string;
   createdAt: string;
 }
 
-const MyNotificationItem = ({ comment, createdAt, seen }: Props) => {
+const MyNotificationItem = ({ comment, message, postId, createdAt }: Props) => {
   const createdDate = formatDate(createdAt);
 
-  const commentBody = JSON.parse(comment);
-  const { content, nickname } = commentBody;
+  if (comment) {
+    const commentBody = JSON.parse(comment);
+    const { content } = commentBody;
+    return <MyNotificationContent content={content} createdDate={createdDate} postId={postId} />;
+  } else if (message) {
+    const messageBody = JSON.parse(message);
+    const { channelName, postId: mentionPostId, content } = messageBody;
+    return (
+      <MyNotificationContent
+        createdDate={createdDate}
+        content={content}
+        channelName={channelName}
+        postId={mentionPostId}
+      />
+    );
+  }
+};
 
+export default MyNotificationItem;
+
+interface MyNotificationContentProps {
+  createdDate: string;
+  content: string;
+  channelName?: string;
+  postId: string;
+}
+const MyNotificationContent = ({
+  createdDate,
+  content,
+  channelName,
+  postId,
+}: MyNotificationContentProps) => {
+  const title = channelName ? `#${channelName}에서 멘션이 왔어요!` : "댓글이 달렸어요!";
   return (
     <li>
       <div className="flex items-center justify-between gap-6 pt-6">
-        <p className="pb-1 text-sm text-muted-foreground">
-          {nickname}님에게 알림이 왔어요! {seen ? "" : "(안읽음)"}
-        </p>
+        <p className="pb-1 text-sm text-muted-foreground">{title}</p>
         <p className="text-xs font-extralight">{createdDate}</p>
       </div>
       <div className="mb-1">{content}</div>
+      <div>postId : {postId}</div>
       <Separator />
     </li>
   );
 };
-
-export default MyNotificationItem;
