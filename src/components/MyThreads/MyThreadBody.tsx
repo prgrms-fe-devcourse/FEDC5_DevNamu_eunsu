db79df12a012b49b3b0e74bb89f7128d6cef744b
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-09 11:58:52 +0900

    Feat: 내가 쓴 글 및 댓글을 comment속성 유무에 따라 post인지 comment인지 구분하여 ThreadItem에 넘겨줌

--- src/components/MyThreads/MyThreadBody.tsx
+++ src/components/MyThreads/MyThreadBody.tsx
@@ -2,12 +2,8 @@ import { parseTitle } from "@/utils/parsingJson";
 
 import MyThreadItem from "./MyThreadItem";
 
-import useGetMyThread from "@/apis/mythreads/useMyThreadQuery";
 import useGetUserInfo from "@/apis/auth/useGetUserInfo";
-
-const Date = ({ date }: { date?: string }) => {
-  return <p className="text-m mt-40pxr py-1pxr font-bold">{date}</p>;
-};
+import useListedThreadsAndComments from "@/hooks/api/useListedThreadsAndComments";
 
 const DEFAULT_VALUE = "658f0f92c31af67084101253";
 //TODO: Comments는 API에서 GET으로 받아올 수 없는 문제 처리 논의 (2023.12.30)
@@ -15,27 +11,27 @@ const DEFAULT_VALUE = "658f0f92c31af67084101253";
 const MyThreadBody = () => {
   const { user } = useGetUserInfo();
   //TODO: 현재는 비로그인 시에도 list목록이 보이게 id 직접 넣어줬지만, 비로그인시 동작 논의(2024.01.02)
-  const id = user ? user._id : DEFAULT_VALUE;
-  const { myThreads, isPending } = useGetMyThread(id);
 
-  if (isPending || myThreads === undefined) {
-    return <span>Loading...</span>;
+  const id = user ? user._id : DEFAULT_VALUE;
+  const { listedThreadsAndComments, isPending } = useListedThreadsAndComments(id);
+  if (isPending) {
+    return <div>loading...</div>;
   }
 
   return (
     <main className="p-10pxr">
-      <Date date="날짜 구분 필요..." />
-      {myThreads?.map(({ _id, channel, title, createdAt }) => {
+      {listedThreadsAndComments?.map(({ _id, channel, title, comment, createdAt }) => {
         // TODO: 파싱하는 로직은 뷰에서 분리하기 (2024.01.04)
-        const { content } = parseTitle(title);
-
+        const { content } = title ? parseTitle(title) : "";
+        const type = comment ? "comment" : "post";
         return (
           <MyThreadItem
-            key={_id}
-            type="post"
-            channel={channel.name}
+            id={_id}
+            type={type}
+            channel={channel?.name}
             title={content}
             createdAt={createdAt}
+            comment={comment}
           ></MyThreadItem>
         );
       })}
