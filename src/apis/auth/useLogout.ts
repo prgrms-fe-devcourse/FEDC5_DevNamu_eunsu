9d50defe50cea35eb403be986a06b453c3820b1d
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-09 10:45:50 +0900

    Feat: 로그아웃 성공 시 스토리지 토큰, 캐싱된 유저 정보 삭제

--- src/apis/auth/useLogout.ts
+++ src/apis/auth/useLogout.ts
@@ -1,10 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { getLocalStorage, removeLocalStorage } from "@/utils/localStorage";
 
 import { postLogout } from "./queryFn";
+import auth from "./queryKey";
 
 const useLogout = () => {
+  const queryClient = useQueryClient();
+  const token = getLocalStorage("token", "");
+
   return useMutation({
     mutationFn: () => postLogout(),
+    onSuccess: () => {
+      queryClient.removeQueries({ queryKey: auth.userInfo(token).queryKey })
+      removeLocalStorage("token");
+    }
   })
 };
 
