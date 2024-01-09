89230a56c486e37e0ce3a0ac2b858ad9bb1bec26
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-09 14:32:03 +0900

    Feat: 사이드바 배열에 requireAuth 프로퍼티 추가

--- src/components/Layout/Sidebar/config.ts
+++ src/components/Layout/Sidebar/config.ts
@@ -5,15 +5,18 @@ export const LINKS = [
     url: "/",
     icon: HomeIcon,
     name: "홈",
+    requireAuth: false,
   },
   {
     url: "/my-notifications",
     icon: BellRingIcon,
     name: "내 알림",
+    requireAuth: true
   },
   {
     url: "/my-threads",
     icon: PenSquareIcon,
     name: "내가 쓴 글",
+    requireAuth: true
   },
 ];
