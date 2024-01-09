53624e7cae6a5bc24e95a421ba5ec81043633210
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-09 09:44:27 +0900

    Feat: 유저 정보 수정 후 유저 정보 갱신

--- src/apis/auth/usePutProfile.ts
+++ src/apis/auth/usePutProfile.ts
@@ -1,10 +1,21 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { getLocalStorage } from "@/utils/localStorage";
 
 import { putUserInfo, putUserPassword } from "./queryFn";
+import auth from "./queryKey";
 
 const usePutProfile = () => {
+  const queryClient = useQueryClient();
+  const token = getLocalStorage("token", "");
+
   const { data: userResponse, mutate: profileChangeMutate } = useMutation({
     mutationFn: (userInfo: string) => putUserInfo(userInfo),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: auth.userInfo(token).queryKey,
+      });
+    },
   });
 
   const { data: passwordResponse, mutate: passwordChangeMutate } = useMutation({
