4c5dc8c0e896cd96868a0de2717d20b04f70bddd
Kim-Jaemin420 <jany240@naver.com>
2024-01-09 13:52:36 +0900

    Docs: 낙관적 업데이트 관련 주석 추가

--- src/apis/thread/useDeleteThreadLike.ts
+++ src/apis/thread/useDeleteThreadLike.ts
@@ -7,6 +7,7 @@ const useDeleteThreadLike = (channelId: string) => {
   const queryClient = useQueryClient();
 
   const { mutate, isPending, isError } = useMutation({
+    // TODO: optimistic updates 구현 (2024.01.09)
     mutationFn: (likeId: string) => deleteThreadLike(likeId),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: threads.threadsByChannel(channelId).queryKey });
