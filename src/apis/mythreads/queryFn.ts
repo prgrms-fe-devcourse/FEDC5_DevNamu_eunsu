bed60b65d52c303bc266ed1834df4b708e2911cd
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-09 11:53:14 +0900

    Feat: queryFn에 댓글 수집을 위한 getUser api함수 생성

--- src/apis/mythreads/queryFn.ts
+++ src/apis/mythreads/queryFn.ts
@@ -1,6 +1,9 @@
 import api from "@/apis/core";
 
 import { Thread } from "@/types/thread";
+import { User } from "@/types/user";
 
 export const getMyThreads = (authorId: string) =>
   api.get<Thread[]>({ url: `/posts/author/${authorId}` });
+
+export const getUser = (authorId: string) => api.get<User>({ url: `/users/${authorId}` });
