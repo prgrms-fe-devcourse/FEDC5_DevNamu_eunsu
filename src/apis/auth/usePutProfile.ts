095e3009ec9102ac078d986d5704f17811229e14
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-10 00:55:45 +0900

    Feat: 프로필 이미지 변경 훅에 추가

--- src/apis/auth/usePutProfile.ts
+++ src/apis/auth/usePutProfile.ts
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { getLocalStorage } from "@/utils/localStorage";
 
-import { putUserInfo, putUserPassword } from "./queryFn";
+import { postUserProfileImage, putUserInfo, putUserPassword } from "./queryFn";
 import auth from "./queryKey";
 
 const usePutProfile = () => {
@@ -21,7 +21,19 @@ const usePutProfile = () => {
   const { data: passwordResponse, mutate: updatePassword } = useMutation({
     mutationFn: (password: string) => putUserPassword(password),
   });
-  return { updateUserName, updatePassword, userResponse, passwordResponse };
+
+  const { data: imageResponse, mutate: updateProfileImage } = useMutation({
+    mutationFn: (image: string) => postUserProfileImage(image),
+  });
+
+  return {
+    updateUserName,
+    updatePassword,
+    updateProfileImage,
+    imageResponse,
+    userResponse,
+    passwordResponse,
+  };
 };
 
 export default usePutProfile;
