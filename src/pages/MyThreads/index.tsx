8e4fd1ec432eec5d90a29b52653d9eb3bd1944db
Kim-Jaemin420 <jany240@naver.com>
2024-01-10 16:41:47 +0900

    Feat: 선택된 리스트 아이디에 맞는 스레드 요청
    Co-authored-by: junly21 <jaejoon0721@ajou.ac.kr>

--- src/pages/MyThreads/index.tsx
+++ src/pages/MyThreads/index.tsx
@@ -1,11 +1,23 @@
+import useThreadStore from "@/stores/thread";
+
 import MyThreadDescription from "@/components/MyThreads/MyThreadDescription";
 import MyThreadBody from "@/components/MyThreads/MyThreadBody";
+import useGetThread from "@/apis/thread/useGetThread";
+import ThreadDetailView from "@/components/common/thread/ThreadDetailView";
 
 const MyThreadsPage = () => {
+  const { selectedThreadId, selectThreadId } = useThreadStore((state) => state);
+  const { thread } = useGetThread(selectedThreadId);
+
+  const handleCloseThreadDetail = () => {
+    selectThreadId(undefined);
+  };
+
   return (
     <div className="h-screen overflow-auto p-30pxr">
       <MyThreadDescription />
       <MyThreadBody />
+      {thread && <ThreadDetailView thread={thread} onClose={handleCloseThreadDetail} />}
     </div>
   );
 };
