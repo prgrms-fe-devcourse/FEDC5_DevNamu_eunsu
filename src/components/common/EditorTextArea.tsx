830e073f37752d953354d95a6168609fc280df54
hun2__2 <wognskec@hanyang.ac.kr>
2024-01-11 01:49:51 +0900

    [#105] Feat: QA 이후 배포 전 EditerTextArea 수정사항 반영 (#112)

--- src/components/common/EditorTextArea.tsx
+++ src/components/common/EditorTextArea.tsx
@@ -1,8 +1,11 @@
 import { useForm } from "react-hook-form";
 import { SendHorizontal } from "lucide-react";
 import { FormEvent, useEffect, useState } from "react";
+import { toast } from "sonner";
 
 import { Textarea } from "@/components/ui/textarea.tsx";
+
+import { getLocalStorage } from "@/utils/localStorage.ts";
 
 import { cn } from "@/lib/utils";
 import MentionInput from "@/components/common/mention/MentionInput.tsx";
@@ -10,6 +13,9 @@ import useEditorLogicByProps, { EditorProps } from "@/hooks/api/useEditorLogicBy
 import { ANONYMOUS_NICKNAME } from "@/constants/anonymousNickname.ts";
 import { UserDBProps } from "@/hooks/api/useUserListByDB.ts";
 import RegisterModal from "@/components/Layout/Modals/Register";
+import useGetUserInfo from "@/apis/auth/useGetUserInfo.ts";
+import LoginModal from "@/components/Layout/Modals/Login";
+import SettingModal from "@/components/Layout/Modals/Setting";
 
 export interface FormValues {
   anonymous: boolean;
@@ -23,6 +29,9 @@ interface Props {
 }
 
 const EditorTextArea = ({ isMention, nickname, editorProps }: Props) => {
+  // TODO: [24/1/10] user는 EditerTextArea를 사용하는 쪽에서 보내주는게 맞다고 생각하지만 빠른 배포를 위해 여기서 불러쓸게요
+  const { user, isPending } = useGetUserInfo();
+
   const [mentionList, setMentionList] = useState<Array<UserDBProps>>([]);
 
   const { upload } = useEditorLogicByProps({
@@ -36,6 +45,18 @@ const EditorTextArea = ({ isMention, nickname, editorProps }: Props) => {
   });
 
   const handleUpload = (formValues: FormValues) => {
+    if (!user) {
+      // TODO: [24/1/11] 이거 나중에 함수로 빼는게 좋을듯해요!
+      toast("로그인 한 유저만 글 쓰기가 가능합니다.", {
+        action: {
+          label: "로그인",
+          onClick: () => setRegisterModalOpen((prev) => !prev),
+        },
+        duration: 2000,
+      });
+
+      return;
+    }
     upload(formValues);
     setMentionList([]);
     setValue("content", "");
@@ -47,23 +68,28 @@ const EditorTextArea = ({ isMention, nickname, editorProps }: Props) => {
   }, []);
 
   // TODO: [24/1/6] 모달 창은 layout단에 위치 시키고 open 여부를 전역상태관리하며 여기서는 트리거 역할만 하기 제안하기, 승인 시 아래 제거(by 성빈님)
+  const [loginModalOpen, setLoginModalOpen] = useState(false);
   const [registerModalOpen, setRegisterModalOpen] = useState(false);
+  const [settingModalOpen, setSettingModalOpen] = useState(false);
+  const isLoggedIn = !!getLocalStorage("token", "");
   const handleClickCheckBox = (e: FormEvent<HTMLInputElement>) => {
     if (!e.currentTarget.checked && nickname === ANONYMOUS_NICKNAME) {
       setValue("anonymous", true);
-      setRegisterModalOpen((prev) => !prev);
+      setSettingModalOpen((prev) => !prev);
       return;
     }
   };
 
+  if (isPending) return <div>로딩 중... </div>;
+
   return (
-    <div className="flex w-full flex-col gap-1">
+    <div className="flex w-full flex-col gap-1 ">
       {isMention && <MentionInput mentionList={mentionList} onChoose={setMentionList} />}
 
       <form className="relative">
         <Textarea
-          placeholder={`내용을 작성해주세요.`}
-          className="resize-none"
+          placeholder={user ? `내용을 작성해주세요.` : "로그인이 필요합니다."}
+          className="resize-none text-base"
           {...register("content")}
         />
         <div className="absolute bottom-2 right-2 flex items-center gap-2">
@@ -92,11 +118,21 @@ const EditorTextArea = ({ isMention, nickname, editorProps }: Props) => {
       </form>
 
       {/*TODO: [24/1/6] 모달 창은 layout단에 위치 시키고 open 여부를 전역상태관리하며 여기서는 트리거 역할만 하기 제안하기, 승인 시 아래 제거(by 성빈님)*/}
-      <RegisterModal
-        open={registerModalOpen}
-        toggleOpen={setRegisterModalOpen}
-        openLoginModal={() => {}}
-      />
+      {!isLoggedIn && (
+        <LoginModal
+          open={loginModalOpen}
+          toggleOpen={setLoginModalOpen}
+          openRegisterModal={setRegisterModalOpen}
+        />
+      )}
+      {!isLoggedIn && (
+        <RegisterModal
+          open={registerModalOpen}
+          toggleOpen={setRegisterModalOpen}
+          openLoginModal={setLoginModalOpen}
+        />
+      )}
+      {isLoggedIn && <SettingModal open={settingModalOpen} toggleOpen={setSettingModalOpen} />}
     </div>
   );
 };
