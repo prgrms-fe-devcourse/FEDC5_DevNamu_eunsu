ba9e34a12fd1e4972f7eea33b721656a5c92c676
Kim-Jaemin420 <jany240@naver.com>
2024-01-11 01:02:24 +0900

    Feat: 댓글 추가시 바로 반영되도록 invalidate 추가

--- src/apis/comment/usePostComment.ts
+++ src/apis/comment/usePostComment.ts
@@ -1,8 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import threads from "../thread/queryKey";
 
 import { createComment } from "@/apis/comment/queryFn.ts";
 
 export const usePostComment = () => {
+  const queryClient = useQueryClient();
+
   // TODO: [24/1/2] error handling 공통으로 뺼 것
-  return useMutation({ mutationFn: createComment, onError: () => {} });
+  return useMutation({
+    mutationFn: createComment,
+    onSuccess: (comment) => {
+      console.log(comment);
+
+      queryClient.invalidateQueries({ queryKey: threads.threadDetail(comment.post).queryKey });
+    },
+    onError: () => {},
+  });
 };
