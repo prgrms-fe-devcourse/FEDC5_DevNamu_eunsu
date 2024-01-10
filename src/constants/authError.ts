0a3fec0c9cdb82ecec385d8d90cf0eda94d61a41
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-10 17:59:42 +0900

    Feat: 회원가입 시 서버 에러 메시지, 프론트 에러 메시지 상수 설정

---
+++ src/constants/authError.ts
@@ -0,0 +1,8 @@
+export const AUTH_ERROR_RESPONSE = {
+  ALREADY_USED_EMAIL: "The email address is already being used.",
+};
+
+export const AUTH_ERROR_MESSAGE = {
+  UNKNOWN_NAME: "없는 프롱이 입니다.",
+  ALREADY_USED_EMAIL: "이미 사용중인 이메일 입니다.",
+};
