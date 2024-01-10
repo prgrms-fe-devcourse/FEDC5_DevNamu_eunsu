e082f4a29726749f2de2047702840898a5665e0b
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-10 20:24:28 +0900

    Refactor: 필요없는 parseUser 로직 삭제

--- src/apis/auth/useLogin.ts
+++ src/apis/auth/useLogin.ts
@@ -2,34 +2,17 @@ import { useMutation } from "@tanstack/react-query";
 
 import { setLocalStorage } from "@/utils/localStorage";
 
-import { postLogin, LoginRequest, AuthResponse } from "./queryFn";
+import { postLogin, LoginRequest } from "./queryFn";
 
 interface Props {
   toggleOpen: (open: boolean) => void;
 }
 
 const useLogin = ({ toggleOpen }: Props) => {
-  const parseUser = (data: AuthResponse) => {
-    const {
-      token,
-      user: { _id: id, email, fullName },
-    } = data;
-    const { name, nickname } = JSON.parse(fullName);
-    return {
-      id,
-      email,
-      name,
-      nickname,
-      token,
-      isLoggedIn: true,
-    };
-  };
-
   return useMutation({
     mutationFn: (body: LoginRequest) => postLogin(body),
-    onSuccess: (data) => {
-      const user = parseUser(data);
-      setLocalStorage("token", user.token);
+    onSuccess: ({ token }) => {
+      setLocalStorage("token", token);
       toggleOpen(false);
     },
   });
