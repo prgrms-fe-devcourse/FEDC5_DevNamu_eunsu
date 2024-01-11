9b3fb5a6d03db6fdef1ba6f9d9e67cec04749bf4
Kim-Jaemin420 <jany240@naver.com>
2024-01-12 04:33:35 +0900

    Feat: 요청 성공시 상태 invalidate 추가

--- src/apis/thread/usePutThread.ts
+++ src/apis/thread/usePutThread.ts
@@ -1,10 +1,23 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import threads from "./queryKey";
 
 import { patchThread } from "@/apis/thread/queryFn.ts";
 
 const usePutThread = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: patchThread,
+    onSuccess: (threadResponse, parameters) => {
+      queryClient.invalidateQueries({
+        queryKey: threads.threadsByChannel(threadResponse.channel?._id).queryKey,
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: threads.threadDetail(parameters.postId).queryKey,
+      });
+    },
     onError: () => {},
   });
 };
