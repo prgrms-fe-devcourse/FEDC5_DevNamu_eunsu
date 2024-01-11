37937aeaaa66a9cbdde091a24f4c9dad4cde09ce
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-11 12:57:13 +0900

    Feat: 유저정보 유무에 따라 있을 경우에 hasNewNotification props 전송유무 구분

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -2,6 +2,8 @@ import { useLocation } from "react-router-dom";
 
 import { SidebarView } from "./view";
 
+import useGetUserInfo from "@/apis/auth/useGetUserInfo";
+
 // TODO: [2023-12-29] 사용자 정보를 Custom Hook으로 받아와 출력하기
 const user = {
   nickname: "익명의 프롱이",
@@ -10,22 +12,28 @@ const user = {
 
 // TODO: [2023-12-29] Notification API를 받아와 출력하기
 // Notification 같은 경우는 알림 페이지에서 확인 시 Notification 확인 여부가 갱신되므로 전역 상태로 관리하는 게 좋을 듯
-const numberOfNotifications = 1;
 
 // TODO: [2023-12-29] 전역 상태 혹은 반영구 저장소에서 현재 테마 가져와 표시하기 (SunIcon or MoonIcon)
 const theme = "light";
 
 const Sidebar = () => {
   const { pathname } = useLocation();
+  const userInfo = useGetUserInfo();
 
-  return (
-    <SidebarView
-      pathname={pathname}
-      user={user}
-      numberOfNotifications={numberOfNotifications}
-      theme={theme}
-    />
-  );
+  if (userInfo.user !== undefined && userInfo.user !== null) {
+    const hasNewNotificaiton = userInfo.user.notifications.length > 0 ? true : false;
+    console.log("hasNewNotificaiton", hasNewNotificaiton);
+    return (
+      <SidebarView
+        pathname={pathname}
+        user={user}
+        hasNewNotification={hasNewNotificaiton}
+        theme={theme}
+      />
+    );
+  } else {
+    return <SidebarView pathname={pathname} user={user} theme={theme} />;
+  }
 };
 
 export default Sidebar;
