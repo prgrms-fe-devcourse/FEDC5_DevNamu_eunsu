0a8f81784dadec89574a85805c457680c5abdd6b
hun2__2 <wognskec@hanyang.ac.kr>
2024-01-12 04:14:51 +0900

    Modify: ThreadDetailView.tsx layout 수정
    1. commentListItem h속성 삭제
    2. editor 그냥 댓글 밑에 붙임

--- src/components/common/EditorTextArea.tsx
+++ src/components/common/EditorTextArea.tsx
@@ -97,7 +97,7 @@ const EditorTextArea = ({ isMention, nickname, editorProps, onClose }: Props) =>
   if (isPending) return <div>로딩 중... </div>;
 
   return (
-    <div className="flex w-full flex-col gap-1 ">
+    <div className="flex w-full flex-col gap-1">
       {isMention && <MentionInput mentionedList={mentionedList} onChoose={setMentionedList} />}
 
       <form className="relative">
