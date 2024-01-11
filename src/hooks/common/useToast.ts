dd5057126e06341c33e418d66d93d6240b5b8703
Kim-Jaemin420 <jany240@naver.com>
2024-01-11 11:06:30 +0900

    Feat: useToast 훅 구현

---
+++ src/hooks/common/useToast.ts
@@ -0,0 +1,26 @@
+import { toast } from "sonner";
+
+interface ToastParameters {
+  message: string;
+  duration: number;
+  actionLabel: string;
+  onActionClick: () => void;
+}
+
+const useToast = () => {
+  const showToast = ({ message, duration, actionLabel, onActionClick }: ToastParameters) => {
+    toast(message, {
+      action: actionLabel
+        ? {
+            label: actionLabel,
+            onClick: onActionClick,
+          }
+        : undefined,
+      duration,
+    });
+  };
+
+  return { showToast };
+};
+
+export default useToast;
