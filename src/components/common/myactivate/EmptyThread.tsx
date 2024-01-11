ed03d306aa3231b6d125b807e4951f1a750b7bde
Kim-Jaemin420 <jany240@naver.com>
2024-01-11 19:00:03 +0900

    Feat: props로 classname 받아와 적용

--- src/components/common/myactivate/EmptyThread.tsx
+++ src/components/common/myactivate/EmptyThread.tsx
@@ -1,10 +1,15 @@
+import { cn } from "@/lib/utils";
+
 interface Props {
   type: string;
+  className?: string;
 }
 
-const EmptyThread = ({ type }: Props) => {
+const EmptyThread = ({ type, className }: Props) => {
   return (
-    <div className="mt-[-170px] flex h-screen flex-col items-center justify-center">
+    <div
+      className={cn("mt-[-170px] flex h-screen flex-col items-center justify-center", className)}
+    >
       <div className=" text-9xl font-bold text-gray-300">텅...</div>
       <div className="mt-20 text-4xl font-bold text-gray-300">
         {type === "notification" ? "아직 알림이 없어요!" : "어서 글을 작성해주세요!"}
