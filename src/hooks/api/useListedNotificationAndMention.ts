693355c42379ab1779c984443fda802382caf873
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-11 12:11:27 +0900

    Feat: 새 알림이 있을 시 전역에서 새로운 알림의 boolean 값을 true로 설정하도록 구현

--- src/hooks/api/useListedNotificationAndMention.ts
+++ src/hooks/api/useListedNotificationAndMention.ts
@@ -1,7 +1,11 @@
+import useNotificationStore from "@/stores/notificaitons";
+
 import useGetNotification from "@/apis/mynotification/useGetNotification";
 import useGetMentioned from "@/apis/mynotification/useGetMentioned";
 
 const useListedNotificationAndMention = () => {
+  const { hasNewNotification, checkNotification } = useNotificationStore();
+
   const { myNotifications, isPending: notificationPending } = useGetNotification();
   const { myMentions, isPending: mentionPending } = useGetMentioned();
   const listedNotificationAndMention = [];
@@ -13,7 +17,15 @@ const useListedNotificationAndMention = () => {
   listedNotificationAndMention.sort(
     (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
   );
-
+  console.log(listedNotificationAndMention);
+  const checkNew = listedNotificationAndMention.some((item) => !item.seen);
+  console.log(checkNew);
+  if (checkNew) {
+    checkNotification(true);
+    console.log("새 알림이 있나요~", hasNewNotification);
+  } else {
+    console.log("새 알림이 없나요~", hasNewNotification);
+  }
   return {
     listedNotificationAndMention,
     isPending: notificationPending || mentionPending,
