dfb1e610719be2dc5eddbb938ce977631b1e0b2c
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-13 22:08:47 +0900

    Feat: 이미지 미리보기 구현

--- src/components/Layout/Modals/Profile/ImageUploadForm.tsx
+++ src/components/Layout/Modals/Profile/ImageUploadForm.tsx
@@ -1,24 +1,46 @@
 import { Image, ImageOff } from "lucide-react";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 
 const ImageUploadForm = () => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [previewImage, setPreviewImage] = useState("");
+
   const handleClickUpload = () => {
     if (!inputRef.current) return;
     inputRef.current.click();
   };
 
+  const handleUploadImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const image = event.target.files?.[0];
+    if (image) {
+      const imagePreviewUrl = URL.createObjectURL(image);
+      setPreviewImage(imagePreviewUrl);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (previewImage) URL.revokeObjectURL(previewImage);
+    };
+  }, [previewImage]);
 
   return (
     <div className="flex flex-col items-center">
-      <img className="h-40 w-40 rounded-sm bg-gray-200" />
       <input
+        onChange={handleUploadImage}
         ref={inputRef}
         type="file"
         accept="image/*"
         className="hidden"
+      />
+      <img
+        src={previewImage || "/svg/userProfile.svg"}
+        className={cn(
+          "h-40 w-40 rounded-full bg-gray-200 object-cover",
+          !previewImage ? "bg-white" : "",
+        )}
       />
       <Button className="my-2" onClick={handleClickUpload}>
         <Image className="mr-2" />
