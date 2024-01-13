aa9c9524d3a5be21fe7202a3420737e67af27345
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-13 22:07:48 +0900

    Feat: 사진 업로드 버튼 클릭 시 inputRef과 연결

--- src/components/Layout/Modals/Profile/ImageUploadForm.tsx
+++ src/components/Layout/Modals/Profile/ImageUploadForm.tsx
@@ -5,12 +5,22 @@ import { Button } from "@/components/ui/button";
 
 const ImageUploadForm = () => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const handleClickUpload = () => {
+    if (!inputRef.current) return;
+    inputRef.current.click();
+  };
+
 
   return (
     <div className="flex flex-col items-center">
-      <input ref={inputRef} type="image" className="hidden" />
       <img className="h-40 w-40 rounded-sm bg-gray-200" />
-      <Button className="my-2">
+      <input
+        ref={inputRef}
+        type="file"
+        accept="image/*"
+        className="hidden"
+      />
+      <Button className="my-2" onClick={handleClickUpload}>
         <Image className="mr-2" />
         사진 업로드
       </Button>
