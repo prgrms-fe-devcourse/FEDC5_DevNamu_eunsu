9d1ed52458dcaee9deb292fd062cc7f521211a67
Kim-Jaemin420 <jany240@naver.com>
2024-01-14 20:52:47 +0900

    Feat: 스레드 리스트와 스레드 상세보기 쿼리키 초기화

--- src/apis/thread/useDeleteThreadLike.ts
+++ src/apis/thread/useDeleteThreadLike.ts
@@ -1,15 +1,91 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import { Thread } from "@/types/thread";
+
 import { deleteThreadLike } from "./queryFn";
 import threads from "./queryKey";
 
-const useDeleteThreadLike = (channelId: string) => {
+interface Parameters {
+  channelId: string;
+  threadId: string;
+}
+
+const useDeleteThreadLike = ({ channelId, threadId }: Parameters) => {
   const queryClient = useQueryClient();
 
   const { mutate, isPending, isError } = useMutation({
     mutationFn: (likeId: string) => deleteThreadLike(likeId),
-    onSuccess: (like) => {
-      queryClient.invalidateQueries({ queryKey: threads.threadDetail(like.post).queryKey });
+    onMutate: async (likeId) => {
+      await queryClient.cancelQueries({
+        queryKey: threads.threadDetail(threadId).queryKey,
+      });
+      await queryClient.cancelQueries({
+        queryKey: threads.threadsByChannel(channelId).queryKey,
+      });
+
+      const previousThread = queryClient.getQueryData<Thread>(
+        threads.threadDetail(threadId).queryKey,
+      );
+
+      const previousThreads = queryClient.getQueryData<Thread[]>(
+        threads.threadsByChannel(channelId).queryKey,
+      );
+
+      queryClient.setQueryData(
+        threads.threadDetail(threadId).queryKey,
+        (oldThread: Thread | undefined) => {
+          if (!oldThread) return oldThread;
+
+          return {
+            ...oldThread,
+            likes: oldThread.likes.filter((like) => like._id !== likeId),
+          };
+        },
+      );
+
+      queryClient.setQueryData(
+        threads.threadsByChannel(channelId).queryKey,
+        (oldThreads: Thread[] | undefined) => {
+          if (!oldThreads) return [];
+
+          return oldThreads.map((thread) =>
+            thread._id === threadId
+              ? { ...thread, likes: thread.likes.filter((like) => like._id !== likeId) }
+              : thread,
+          );
+        },
+      );
+
+      return { previousThread, previousThreads };
+    },
+    onError: (error, _, context) => {
+      queryClient.setQueryData(threads.threadDetail(threadId).queryKey, context?.previousThread);
+      queryClient.setQueryData(
+        threads.threadsByChannel(channelId).queryKey,
+        context?.previousThreads,
+      );
+    },
+
+    onSuccess: (likeResponse) => {
+      queryClient.setQueryData(threads.threadDetail(threadId).queryKey, (oldThread: Thread) => ({
+        ...oldThread,
+        likes: oldThread.likes.filter((like) => like._id !== likeResponse._id),
+      }));
+
+      queryClient.setQueryData(
+        threads.threadsByChannel(channelId).queryKey,
+        (oldThreads: Thread[] | undefined) => {
+          if (!oldThreads) return [];
+
+          return oldThreads.map((thread) =>
+            thread._id === threadId
+              ? { ...thread, likes: thread.likes.filter((like) => like._id !== likeResponse._id) }
+              : thread,
+          );
+        },
+      );
+
+      queryClient.invalidateQueries({ queryKey: threads.threadDetail(likeResponse.post).queryKey });
       queryClient.invalidateQueries({ queryKey: threads.threadsByChannel(channelId).queryKey });
     },
   });
