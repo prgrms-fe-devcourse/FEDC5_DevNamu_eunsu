9c3fe2125a84c002d072bdae1e33080a68d0f911
Kim-Jaemin420 <jany240@naver.com>
2024-01-14 19:52:17 +0900

    Cleanup: 주석 제거

--- src/apis/thread/useDeleteThreadLike.ts
+++ src/apis/thread/useDeleteThreadLike.ts
@@ -7,7 +7,6 @@ const useDeleteThreadLike = (channelId: string) => {
   const queryClient = useQueryClient();
 
   const { mutate, isPending, isError } = useMutation({
-    // TODO: optimistic updates 구현 (2024.01.09)
     mutationFn: (likeId: string) => deleteThreadLike(likeId),
     onSuccess: (like) => {
       queryClient.invalidateQueries({ queryKey: threads.threadDetail(like.post).queryKey });
