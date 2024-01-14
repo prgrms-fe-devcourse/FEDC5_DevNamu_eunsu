2c7446334d4d03bbc4fff8e045cdb7715b9ee716
Kim-Jaemin420 <jany240@naver.com>
2024-01-14 21:05:15 +0900

    Feat: id의 유무에 따라 좋아요, 좋아요 삭제 요청하는 토글 훅 구현

---
+++ src/hooks/api/useToggleLike.ts
@@ -0,0 +1,37 @@
+import * as Sentry from "@sentry/react";
+
+import { getLocalStorage } from "@/utils/localStorage";
+
+import usePostThreadLike from "@/apis/thread/usePostThreadLike";
+import useDeleteThreadLike from "@/apis/thread/useDeleteThreadLike";
+
+interface Parameters {
+  channelId: string;
+  threadId: string;
+}
+
+const useToggleLike = ({ channelId, threadId }: Parameters) => {
+  const token = getLocalStorage("token", "");
+  const { likeThread, isPending: isLikePending } = usePostThreadLike(channelId);
+  const { removeLike, isPending: isUnLikePending } = useDeleteThreadLike({ channelId, threadId });
+
+  const toggleLike = (likeId: string | undefined) => {
+    if (!token) return;
+
+    if (isLikePending || isUnLikePending) return;
+
+    if (!likeId) {
+      likeThread(threadId);
+      Sentry.captureMessage("ui 사용 - 좋아요 등록", "info");
+    }
+
+    if (likeId) {
+      removeLike(likeId);
+      Sentry.captureMessage("ui 사용 - 좋아요 삭제", "info");
+    }
+  };
+
+  return { toggleLike, isLikePending, isUnLikePending };
+};
+
+export default useToggleLike;
