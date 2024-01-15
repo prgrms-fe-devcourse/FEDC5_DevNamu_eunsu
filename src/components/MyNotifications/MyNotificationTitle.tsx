676e54d4dc1a748752a9b1d7d633d10ba9045561
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-15 11:58:13 +0900

    Modify: 내 알림  헤더 변경

--- src/components/MyNotifications/MyNotificationTitle.tsx
+++ src/components/MyNotifications/MyNotificationTitle.tsx
@@ -1,10 +1,10 @@
 const MyNotificationTitle = () => {
   return (
-    <div>
-      <div className="flex items-center gap-6 pb-4">
-        <span className="h-[30px] text-2xl font-bold">내 알림</span>
-      </div>
-      <hr />
+    <div className="w-100 mb-2 h-auto rounded-md border p-4 shadow-md">
+      <p className="text-lg font-bold">내 알림</p>
+      <p className="mt-2 text-base font-normal">
+        내 글에 달린 댓글, 좋아요, 나에게 달린 멘션을 확인할 수 있어요.
+      </p>
     </div>
   );
 };
