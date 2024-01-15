0486e5725ea9c75467ab448dad36383d03fec43d
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-15 13:03:11 +0900

    Style: 스켈레톤 색상 변경

--- src/components/Skelton/ThreadSkeleton.tsx
+++ src/components/Skelton/ThreadSkeleton.tsx
@@ -4,16 +4,16 @@ const CustomThreadSkeleton = () => {
   return (
     <li className="relative mb-5 px-2.5 py-2.5 ">
       <div className="flex">
-        <Skeleton className="mr-3 flex h-12 w-12 shrink-0 overflow-hidden rounded-full" />
+        <Skeleton className="mr-3 flex h-12 w-12 shrink-0 overflow-hidden rounded-full bg-gray-200" />
         <div className="min-w-0 flex-grow">
           <div className="flex justify-between">
-            <Skeleton className="h-6 w-[120px]" />
-            <Skeleton className="h-6 w-[120px]" />
+            <Skeleton className="h-6 w-[120px] bg-gray-200" />
+            <Skeleton className="h-6 w-[120px] bg-gray-200" />
           </div>
-          <Skeleton className="mb-4 mt-2 h-8 w-[750px]" />
+          <Skeleton className="mb-4 mt-2 h-8 w-[750px] bg-gray-200" />
           <div className="flex">
-            <Skeleton className="mr-2 h-5 w-[40px]" />
-            <Skeleton className="h-5 w-[120px]" />
+            <Skeleton className="mr-2 h-5 w-[40px] bg-gray-200" />
+            <Skeleton className="h-5 w-[120px] bg-gray-200" />
           </div>
         </div>
       </div>
