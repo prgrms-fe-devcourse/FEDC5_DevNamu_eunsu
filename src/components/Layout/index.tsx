4e08c656bad40c77b4067b4cfd30cc3cce13800a
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-15 22:08:15 +0900

    Feat: demo를 위한 컴포넌트 import

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 
 import Sidebar from "./Sidebar";
+import InformationModal from "./Modals/Information";
 
 const Layout = () => {
   // TODO: [2023-12-29] 테마 설정에 따라 배경색 변경하기
   return (
     <div className="bg flex h-screen w-screen flex-row">
       <Sidebar />
+      {/* // TODO: 머지 전 삭제 */}
+      <InformationModal open={true} toggleOpen={() => {}} />
       <div className="bg-layer-1 w-full">
         <Outlet />
       </div>
