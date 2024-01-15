18417218e4d9b39a5fbb89553c66d3eb3d8a9e55
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-16 03:43:34 +0900

    Refactor: 반복되는 스타일 상수 선언하여 사용

--- src/components/Layout/Modals/Information/index.tsx
+++ src/components/Layout/Modals/Information/index.tsx
@@ -1,11 +1,13 @@
 import SimpleBaseModal from "../Base/modal";
 
-import StrongText from "./StrongText";
+import { cn } from "@/lib/utils";
 
 interface Props {
   open: boolean;
   toggleOpen: (open: boolean) => void;
 }
+
+const titleCSS = "mb-2 mt-5 text-base font-bold text-lime-600";
 
 const InformationModal = ({ open, toggleOpen }: Props) => {
   return (
@@ -20,13 +22,11 @@ const InformationModal = ({ open, toggleOpen }: Props) => {
         데브코스 간 생긴 일에 대해 익명으로 하고 싶었던
         <br />
         칭찬, 응원, 고해성사를 할 수 있는 <StrongText name="데나무숲" />를 소개합니다.
-        <StrongText name="칭찬&응원 게시판 사용법" isTitle />
         소중한 사람들을 멘션하여
         <br />
         익명으로 감사 혹은 응원의 마음을 전달해보세요.
         <br />
         본인이 칭찬이나 응원을 받고 싶을 때도 사용할 수 있습니다!
-        <StrongText name="무능 게시판 사용법" isTitle />
         본인이 무능하다고 여겨질 때,
         <br />
         혹은 아무 것도 안(못)해서 죄책감이 생겼을 때,
@@ -34,11 +34,13 @@ const InformationModal = ({ open, toggleOpen }: Props) => {
         무능 게시판에서 고해성사를 통해
         <br />
         답답한 마음을 풀어내보세요.
-        <StrongText name="개선 사항 게시판 사용법" isTitle />
         원하는 개선사항은
         <br />
         개선사항 채널에 편하게 익명으로 남겨주세요.
         <p className="mt-6 text-sm text-gray-400">
+        <strong className={cn(titleCSS, "block")}>칭찬&응원 게시판 사용법</strong>
+        <strong className={cn(titleCSS, "block")}>무능 게시판 사용법</strong>
+        <strong className={cn(titleCSS, "block")}>개선 사항 게시판 사용법</strong>
           데나무숲을 만든 사람들이 궁금하다면
           <br />
           <a
