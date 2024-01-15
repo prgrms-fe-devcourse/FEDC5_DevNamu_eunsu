aa565d1341bf897555ccef76f02e5f9c5e477e4c
hun2__2 <wognskec@hanyang.ac.kr>
2024-01-16 03:19:02 +0900

    Fix: UserDBProps 변경 배포 오류 해결2

--- src/components/common/mention/UserBadgeList.tsx
+++ src/components/common/mention/UserBadgeList.tsx
@@ -1,15 +1,18 @@
 import { Badge } from "@/components/ui/badge.tsx";
 
 import { ListProps } from "@/components/common/mention/AutoCompleteMentionList";
+import { UserDBProps } from "@/hooks/api/useUserListByDB.ts";
 
 const UserBadgeList = ({ users, onClick }: ListProps) => {
   if (!users.length) return "";
+
+  const handleClick = () => (people: UserDBProps) => onClick(people);
 
   return (
     <div className="mb-2 flex flex-wrap gap-2">
       {users.map(({ name, userId }) => {
         return (
-          <Badge key={userId} onClick={() => onClick({ name, userId })} className="cursor-pointer">
+          <Badge key={userId} onClick={handleClick} className="cursor-pointer">
             {name}
           </Badge>
         );
