a42ac87d0ceb425ff33a080ef365875df960c560
junly21 <jaejoon0721@ajou.ac.kr>
2024-01-15 11:57:23 +0900

    Style: 내가 쓴 글 레이아웃 및 폰트크기 수정

--- src/components/MyThreads/MyPost.tsx
+++ src/components/MyThreads/MyPost.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const MyPost = ({ title }: Props) => {
   return (
-    <li className="max-w-xl">
+    <li className="max-w-xl pb-3">
       <div className="mb-2 overflow-hidden truncate text-lg font-normal">{title}</div>
     </li>
   );
