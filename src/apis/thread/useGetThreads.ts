207515e787ad86e90068a981558f7912f5b7a156
Kim-Jaemin420 <jany240@naver.com>
2024-01-16 11:37:27 +0900

    Modify: rest 파라미터로 내보내기

--- src/apis/thread/useGetThreads.ts
+++ src/apis/thread/useGetThreads.ts
@@ -4,31 +4,25 @@ import { getThreadsByChannelId } from "./queryFn";
 import threads from "./queryKey";
 
 const useGetThreads = (channelId: string | undefined, totalThreads: number) => {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isPending, isError, error } =
-    useInfiniteQuery({
-      queryKey: threads.threadsByChannel(channelId).queryKey,
-      queryFn: channelId
-        ? ({ pageParam = 0 }) => getThreadsByChannelId(channelId, pageParam)
-        : undefined,
-      enabled: !!channelId && !!totalThreads,
-      getNextPageParam: (lastPage) => {
-        if (!lastPage[0]) return;
+  const { data, ...rest } = useInfiniteQuery({
+    queryKey: threads.threadsByChannel(channelId).queryKey,
+    queryFn: channelId
+      ? ({ pageParam = 0 }) => getThreadsByChannelId(channelId, pageParam)
+      : undefined,
+    enabled: !!channelId && !!totalThreads,
+    getNextPageParam: (lastPage) => {
+      if (!lastPage[0]) return;
 
-        const nextPageOffset = lastPage[0].nextPage;
-        if (nextPageOffset > totalThreads) return undefined;
-        return nextPageOffset;
-      },
-      initialPageParam: 0,
-    });
+      const nextPageOffset = lastPage[0].nextPage;
+      if (nextPageOffset > totalThreads) return undefined;
+      return nextPageOffset;
+    },
+    initialPageParam: 0,
+  });
 
   return {
     threads: data?.pages.flatMap((page) => page),
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    isPending,
-    isError,
-    error,
+    ...rest,
   };
 };
 
