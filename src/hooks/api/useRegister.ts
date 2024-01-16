74900bb92230a11a24be8bd4898c192b512eb04c
hun2__2 <wognskec@hanyang.ac.kr>
2024-01-16 13:48:15 +0900

    Modify: 에러 sentry로 throw

--- src/hooks/api/useRegister.ts
+++ src/hooks/api/useRegister.ts
@@ -1,3 +1,7 @@
+import * as Sentry from "@sentry/react";
+
+import { log } from "@/utils/logger.ts";
+
 import usePostRegister from "@/apis/auth/usePostRegister.ts";
 import { RegisterRequest } from "@/apis/auth/queryFn.ts";
 import useUpdateUserList from "@/hooks/api/useUpdateUserList.ts";
@@ -7,15 +11,20 @@ const useRegister = () => {
   const { updateUserList } = useUpdateUserList();
 
   const register = async (registerForm: RegisterRequest) => {
-    const {
-      user: { _id },
-    } = await mutateAsync(registerForm);
+    try {
+      const {
+        user: { _id },
+      } = await mutateAsync(registerForm);
 
-    const { name } = registerForm.fullName;
+      const { name } = registerForm.fullName;
 
-    await updateUserList({ _id, name });
+      await updateUserList({ _id, name });
 
-    return name;
+      return name;
+    } catch (error) {
+      log("error", "Error reading from Register:", error);
+      Sentry.captureException(error);
+    }
   };
 
   return {
