b85184180e70c1604511e639650c6f969b413a3c
Seongbin Kim <seongbin9786@gmail.com>
2024-01-16 11:25:05 +0900

    Chore: GA4 config

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,39 @@
 import { VitePluginRadar } from "vite-plugin-radar";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      include: "**/*.svg",
-    }),
-    sentryVitePlugin({
-      org: "jojaehun",
-      project: "javascript-react",
-      telemetry: false,
-    }),
-    VitePluginRadar({
-      analytics: {
-        id: process.env.VITE_GA4_ID,
-      },
-    }),
-  ],
+// env를 사용하기 위해선 loadEnv(mode)가 필요해 해당 방식을 사용
+// https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
+  return defineConfig({
+    plugins: [
+      react(),
+      svgr({
+        include: "**/*.svg",
+      }),
+      sentryVitePlugin({
+        org: "jojaehun",
+        project: "javascript-react",
+        telemetry: false,
+      }),
+      VitePluginRadar({
+        enableDev: false,
+        analytics: {
+          id: process.env.VITE_GA4_ID,
+        },
+      }),
+    ],
 
-  build: {
-    sourcemap: true,
-  },
-});
+    resolve: {
+      alias: [{ find: "@", replacement: "/src" }],
+    },
+
+    build: {
+      sourcemap: true,
+    },
+  });
+};
