1b71e0bd327d9f00add614b4070551f28b7d5582
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-01-16 11:44:56 +0900

    Cleanup: merge 전 데모 코드 삭제

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,20 +1,14 @@
 import { Outlet } from "react-router-dom";
 
 import Sidebar from "./Sidebar";
-import InformationModal from "./Modals/Information";
 
-const Layout = () => {
-  // TODO: [2023-12-29] 테마 설정에 따라 배경색 변경하기
-  return (
-    <div className="bg flex h-screen w-screen flex-row">
-      <Sidebar />
-      {/* // TODO: 머지 전 삭제 */}
-      <InformationModal open={true} toggleOpen={() => {}} />
-      <div className="bg-layer-1 w-full">
-        <Outlet />
-      </div>
+const Layout = () => (
+  <div className="bg flex h-screen w-screen flex-row">
+    <Sidebar />
+    <div className="bg-layer-1 w-full">
+      <Outlet />
     </div>
-  );
-};
+  </div>
+);
 
 export default Layout;
