70686d5225014643e7d094193627a09df5723aa0
Seongbin Kim <seongbin9786@gmail.com>
2024-01-17 11:34:20 +0900

    Fix: GA4 script를 개발 환경에서는 로딩하지 않는 오류 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,40 @@
 import { VitePluginRadar } from "vite-plugin-radar";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      include: "**/*.svg",
-    }),
-    sentryVitePlugin({
-      org: process.env.SENTRY_ORG,
-      project: process.env.SENTRY_PROJECT,
-      telemetry: false,
-    }),
-    VitePluginRadar({
-      enableDev: false,
-      analytics: {
-        id: process.env.GA4_ID,
-      },
-    }),
-  ],
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
+  console.log(process.env);
 
-  build: {
-    sourcemap: true,
-  },
-});
+  return defineConfig({
+    plugins: [
+      react(),
+      svgr({
+        include: "**/*.svg",
+      }),
+      sentryVitePlugin({
+        org: process.env.SENTRY_ORG,
+        project: process.env.SENTRY_PROJECT,
+        telemetry: false,
+      }),
+      VitePluginRadar({
+        enableDev: true,
+        analytics: {
+          id: process.env.VITE_GA4_ID,
+          disable: process.env.NODE_ENV !== "production",
+        },
+      }),
+    ],
+
+    resolve: {
+      alias: [{ find: "@", replacement: "/src" }],
+    },
+
+    build: {
+      sourcemap: true,
+    },
+  });
+};
