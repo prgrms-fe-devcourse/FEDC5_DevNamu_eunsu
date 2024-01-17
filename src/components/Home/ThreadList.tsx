5372cbac052a53bd58e1da56c44b48d2c560c09f
Kim-Jaemin420 <jany240@naver.com>
2024-01-17 09:50:24 +0900

    Feat: 마지막 스크롤 위치 저장 및 스크롤 기준 데이터 fetching

--- src/components/Home/ThreadList.tsx
+++ src/components/Home/ThreadList.tsx
@@ -1,35 +1,74 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 import useSelectedThreadStore from "@/stores/thread";
 
 import { Thread } from "@/types/thread";
 
 import ThreadListItem from "@/components/common/thread/ThreadListItem";
+import useIntersectionObserver from "@/hooks/common/useIntersectionObserver";
 
 interface Props {
   threads: Thread[];
+  channelName: string;
+  hasNextPage: boolean;
+  isFetchingNextPage: boolean;
+  fetchNextPage: () => void;
 }
 
-const ThreadList = ({ threads }: Props) => {
+const ThreadList = ({
+  isFetchingNextPage,
+  hasNextPage,
+  fetchNextPage,
+  threads,
+  channelName,
+}: Props) => {
+  const threadListItemRef = useRef(null);
   const threadListRef = useRef<HTMLUListElement>(null);
 
+  const handleIntersect = () => {
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  };
   const { selectThreadId } = useSelectedThreadStore((state) => state);
 
   const handleClickThread = (threadId: string) => () => {
     selectThreadId(threadId);
   };
 
-  useEffect(() => {
-    if (threadListRef.current && threads.length > 0) {
-      threadListRef.current.scrollTop = threadListRef.current.scrollHeight;
+  useIntersectionObserver({ target: threadListItemRef, handleIntersect });
+
+  const handleScroll = useCallback(() => {
+    if (threadListRef.current) {
+      localStorage.setItem(
+        `scrollPosition-${channelName}`,
+        String(threadListRef.current.scrollTop),
+      );
     }
-  }, [threads.length]);
+  }, [channelName]);
+
+  useEffect(() => {
+    const savedScrollPosition = localStorage.getItem(`scrollPosition-${channelName}`);
+    if (savedScrollPosition && threadListRef.current) {
+      threadListRef.current.scrollTop = parseInt(savedScrollPosition, 10);
+    }
+
+    if (threadListRef.current) {
+      threadListRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (threadListRef.current) {
+        threadListRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [channelName, handleScroll]);
 
   return (
     <div>
       <ul
         ref={threadListRef}
-        className="flex h-[calc(100vh-250px)] flex-col overflow-y-auto pt-80pxr"
+        className="flex h-[calc(100vh-250px)] flex-col-reverse overflow-auto pt-80pxr"
       >
         {threads.map((thread) => (
           <ThreadListItem
